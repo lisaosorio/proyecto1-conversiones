cb3b2fb19a8cb565a175f7bd8ad6a22188f699f2
lisaosorio <20120186@colegioscj.edu.gt>
2025-05-14 08:15:44 -0600

    Add files via upload

---
+++ Ejercicio.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+ventana = tk.Tk()
+ventana.title("Conversiones")
+ventana.geometry("450x300")
+ventana.configure(bg="#e38dbc")
+
+etiqueta = tk.Label(ventana, text="Escoge una opción")
+
+boton_longitud=tk.Button(ventana, text="Longitud", bg="white")
+boton_longitud.pack()
+
+boton_masa=tk.Button(ventana, text="Masa", bg="white")
+boton_masa.pack()
+
+boton_tiempo=tk.Button(ventana, text="Tiempo", bg="white")
+boton_tiempo.pack()
+ventana.mainloop()
