6486bb9e340a9170d24abdf33ec916840ca2b42f
lisaosorio <20120186@colegioscj.edu.gt>
2025-05-14 07:51:36 -0600

    Correccion de nombre de archivo

---
+++ Ejercicio.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+ventana = tk.Tk()
+ventana.title("Conversiones")
+ventana.geometry("450x300")
+ventana.configure(bg="#e38dbc")
+ventana.mainloop()
