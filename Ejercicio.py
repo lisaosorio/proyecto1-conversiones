8bc330a52a7712d0c159c376eb64cce4f5aec05b
lisaosorio <20120186@colegioscj.edu.gt>
2025-05-14 08:06:28 -0600

    nombre

--- Ejercicio.py
+++ Ejercicio.py
@@ -7,4 +7,9 @@ ventana = tk.Tk()
 ventana.title("Conversiones")
 ventana.geometry("450x300")
 ventana.configure(bg="#e38dbc")
+
+
+
+boton_longitud=tk.Button(ventana, text="Longitud", bg="white")
+
 ventana.mainloop()
